d0c11557518e4d8a3f20df81385de26d80204606
anborgh <divoannet@gmail.com>
2022-06-05 19:03:01 +0300

    Several names per fandom.

--- js/wantedFilters/wantedFilters.js
+++ js/wantedFilters/wantedFilters.js
@@ -3,9 +3,25 @@
  * Список даёт возможность фильтровать тему по фендому или по персонажу.
  *
  * author: Человек-Шаман
- * version: 1.0
+ * version: 1.1
  * status: DONE
- */
+ * 
+ * instruction:
+ * 1. Добавить в html-низ, заменить 999 на номер темы поиска
+ *    <script>
+ *      hvWantedFilters.init(999);
+ *    </script>
+ * 2. Добавить в первое сообщение темы поиска
+ *    [block=charlist][/block]
+ *    там, где нужно отрисовать список ролей
+ * 3. Убедитесь, что [block=charlist][/block] не поставлен в спойлере
+ *    для медиа или html-блоке, так он не будет работать
+ * 4. Для стилизации используйте стили
+ *    .post .charlist { стили блока списка ролей }
+ *    .post .charlist .charlist_fd { стили блока фандома }
+ *    .post .charlist .charlist_item { стили элемента списка фандома }
+ *    .post .charlist .charlist_title { стили заголовка фандома }
+  */
 
 const hvWantedFilters = {
   topicId: 0,
@@ -34,12 +50,12 @@ const hvWantedFilters = {
     this.setNeddfulElements();
     
     this.topicId = topicId;
-    await this.getTopicData();
-    await this.getPosts();
+    await this.getTopicData(topicId);
+    await this.getPosts(topicId);
     this.renderSummary();
 
     this.initList();
-},
+  },
   bindHandlers: function() {
     this.handleListClick = this.handleListClick.bind(this);
   },
@@ -48,18 +64,18 @@ const hvWantedFilters = {
     this.filterList = $('<div class="hvFilteredList"></div>');
     $('.topicpost').after(this.filterList);
   },
-  getTopicData: async function() {
+  getTopicData: async function(topicId) {
     const topicData = await $.get(
-      `/api.php?method=topic.get&topic_id=${this.topicId}`
+      `/api.php?method=topic.get&topic_id=${topicId}`
     );
     const postCount = Number(topicData.response[0]?.num_replies);
     this.topicData.postCount = isNaN(postCount) ? this.topicData.postCount : postCount;
   },
-  getPosts: async function() {
+  getPosts: async function(topicId) {
     const reqestCount = Math.ceil(this.topicData.postCount / 100);
     for (let i = 0; i < reqestCount; i++) {
       const { response } = await $.get(
-        `/api.php?method=post.get&topic_id=${this.topicId}&skip=${i * 100}&limit=100`
+        `/api.php?method=post.get&topic_id=${topicId}&skip=${i * 100}&limit=100`
       );
       this.topicData.posts = this.topicData.posts.concat(response);
     }
@@ -67,28 +83,38 @@ const hvWantedFilters = {
   },
   getFandoms: function() {
     this.topicData.posts.forEach((post) => {
+      if (index === 0) return;
       const message = $(`<div>${post.message}</div>`);
       const $fandomName = message.find(".fd")[0];
-      const $charName = message.find(".nm")[0];
+      const $charNames = message.find(".nm");
   
-      if (!$fandomName || !$charName) return;
-  
-      const fandomName = $($fandomName).text();
-      const charName = $($charName).text();
-  
+      const fandomName = $fandomName ? $($fandomName).text() : 'Other';
+      const charNames = [];
+      $charNames.each((index, item) => charNames.push($(item).text()));
+
       if (!this.fandoms[fandomName.toLowerCase()]) {
         this.fandoms[fandomName.toLowerCase()] = {
           name: fandomName,
           items: [],
         };
       }
-  
-      this.fandoms[fandomName.toLowerCase()].items.push({
-        name: charName,
-        postId: post.id,
-        author: post.username,
-        rating: post.rating,
-      });
+
+      charNames.forEach(name => {
+        this.fandoms[fandomName.toLowerCase()].items.push({
+          name,
+          postId: post.id,
+          author: post.username,
+          rating: post.rating,
+        });
+      })
+      if (charNames.length === 0) {
+        this.fandoms[fandomName.toLowerCase()].items.push({
+          name: null,
+          postId: post.id,
+          author: post.username,
+          rating: post.rating,
+        });
+      }
     });
   },
   renderSummary: function() {
@@ -99,10 +125,14 @@ const hvWantedFilters = {
     $charlist.append('<span class="hvClearFilters">x Сбросить фильтр</span>');
 
     filteredFandomNames.forEach((fandom, index) => {
+      if (fandom === 'other' && this.fandoms[fandom].items.length === 0) {
+        return;
+      }
       const ul = $(`<ul class="charlist_fd fd${index}"></ul>`);
       ul.append(`<li class="charlist_item charlist_title" data-fandom="${fandom}">${this.fandoms[fandom].name}</li>`);
       const sortedNames = this.fandoms[fandom].items.sort((a, b) => a.name > b.name ? 1 : -1);
       sortedNames.forEach((char) => {
+        if (!char.name) return;
         ul.append(
           `<li class="charlist_item" data-character="${char.postId}"><a href="viewtopic.php?pid=${char.postId}#p${char.postId}" data-post-id="${char.postId}" title="by ${char.author}">${char.name}</a></li>`
         );
